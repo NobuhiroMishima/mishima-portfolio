132ba459b7e9bea1c974172dac31338675894c11
nobuhiro.mishima <nobuhiro.mishima@outlook.com>
2024-12-21 17:41:31 +0900

    Service / 一覧画面の作成

---
+++ src/app/service/page.jsx
@@ -0,0 +1,38 @@
+import Headline from "@/components/elements/title/Headline";
+import { servicecontent } from "@/features/service/constants/servicecontent";
+
+function page() {
+  return (
+    <section className="px-28 max-lg:px-16 max-md:px-10 max-xs:px-5 py-16 relative">
+      <div className="max-w-6xl mx-auto">
+        <div className="flex flex-col items-start mb-12">
+          <Headline title={"Service"} subtitle={"サービス"} />
+        </div>
+        <div className="flex flex-col gap-10">
+          <div>
+            <p>
+              サービス内容、料金表の一例になります。お仕事のご依頼・ご相談の際にはページ最下部のお問い合わせからお願いいたします。
+              <br />
+              ※下記の料金表は目安になります。規模や納期によって料金は変動いたします。
+              <br />
+              ※ご不明な点等ございましたらお気軽にご連絡ください。
+            </p>
+          </div>
+          <div className="flex flex-wrap justify-around max-w-[1072px] gap-6">
+            {servicecontent.map((content, index) => (
+                <img
+                key={index}
+                src={content.cardimage}
+                alt={`${content.name} Image`}
+                height={144}
+                className="h-36 object-contain"
+                />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default page;
