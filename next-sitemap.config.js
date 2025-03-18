93cbc1afa139e9032b70614c6fb20e78f917ab47
nobuhiro.mishima <nobuhiro.mishima@outlook.com>
2025-03-18 19:43:13 +0900

    fix: サイトマップの無限ループ修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,9 +2,4 @@ module.exports = {
     siteUrl: 'https://mishimaworks.jp',
     generateRobotsTxt: true,
     exclude: ['/api/*'],
-    robotsTxtOptions: {
-        additionalSitemaps: [
-            'https://mishimaworks.jp/sitemap.xml',
-        ],
-    },
 };
